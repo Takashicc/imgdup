f1164e676af013253d746f21ed49cff5fe8e8c5c
Takashicc <105186894+Takashicc@users.noreply.github.com>
2025-03-20 15:57:24 +0900

    fix: Add CreatedAt, UpdatedAt manually and set default value and set trigger to update updated_at when updating

--- migration/src/m20250310_123143_reference_images.rs
+++ migration/src/m20250310_123143_reference_images.rs
@@ -7,22 +7,53 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .create_table(timestamps(
+            .create_table(
                 Table::create()
                     .table(ReferenceImage::Table)
                     .if_not_exists()
                     .col(pk_auto(ReferenceImage::Id))
                     .col(string(ReferenceImage::Filepath))
                     .col(binary(ReferenceImage::Hash))
+                    .col(timestamp(ReferenceImage::CreatedAt).default(Expr::current_timestamp()))
+                    .col(timestamp(ReferenceImage::UpdatedAt).default(Expr::current_timestamp()))
                     .to_owned(),
+            )
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(sea_orm::Statement::from_string(
+                manager.get_database_backend(),
+                r#"
+                CREATE TRIGGER IF NOT EXISTS reference_image_updated_at
+                AFTER UPDATE ON reference_image
+                FOR EACH ROW
+                BEGIN
+                    UPDATE reference_image
+                    SET updated_at = CURRENT_TIMESTAMP
+                    WHERE id = NEW.id;
+                END
+                "#,
             ))
-            .await
+            .await?;
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
+            .get_connection()
+            .execute(sea_orm::Statement::from_string(
+                manager.get_database_backend(),
+                "DROP TRIGGER IF EXISTS reference_image_updated_at",
+            ))
+            .await?;
+
+        manager
             .drop_table(Table::drop().table(ReferenceImage::Table).to_owned())
-            .await
+            .await?;
+
+        Ok(())
     }
 }
 
@@ -32,4 +63,6 @@ enum ReferenceImage {
     Id,
     Filepath,
     Hash,
+    CreatedAt,
+    UpdatedAt,
 }
